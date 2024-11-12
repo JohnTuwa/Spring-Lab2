ab972aa2fe4a30ffc7720aec75546154533cd3de
Anna <anuta2106200506@gmail.com>
2024-11-12 18:18:45 +0100

    Add FilmRepository

--- src/main/java/com/example/lab2/Repositories/FilmRepository.java
+++ src/main/java/com/example/lab2/Repositories/FilmRepository.java
@@ -1,4 +1,21 @@
 package com.example.lab2.Repositories;
 
-public class FilmRepository {
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class FilmRepository{
+
+    private List<Film> films = new ArrayList<>();
+
+    public void addFilm(Long id,String title,double rating,double ticketPrice,String sessionTime) {
+        Film film = new Film(id,title,rating,ticketPrice,sessionTime);
+        films.add(film);
+    }
+
+    public List<Film> findAll() {
+        return films;
+    }
+
+
 }
